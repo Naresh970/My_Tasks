0fe3b634fa77fa5c76bee34c54f8efc9dbf96c4f
Naresh Kumar <161779541+Naresh970@users.noreply.github.com>
2025-12-15 10:51:07 +0530

    Change database URL to MYDB

--- Elevate Labs Task7/JDBC_DEMO/src/main/java/org/studyeasy/App.java
+++ Elevate Labs Task7/JDBC_DEMO/src/main/java/org/studyeasy/App.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class App 
 {
     public static void main( String[] args ) throws SQLException {
-        String url="jdbc:postgresql://localhost:5432/Telusko";
+        String url="jdbc:postgresql://localhost:5432/MYDB";
         String uname="postgres";
         String password="naresh123";
         //  creating connection
