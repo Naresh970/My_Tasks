0e2546207c9f68f2b877e4d6206faf7df9b14bfe
Naresh Kumar <161779541+Naresh970@users.noreply.github.com>
2026-01-28 10:50:39 +0530

    Update database credentials in App.java

--- Elevate Labs Task7/JDBC_DEMO/src/main/java/org/studyeasy/App.java
+++ Elevate Labs Task7/JDBC_DEMO/src/main/java/org/studyeasy/App.java
@@ -5,8 +5,8 @@ public class App
 {
     public static void main( String[] args ) throws SQLException {
         String url="jdbc:postgresql://localhost:5432/MYDB";
-        String uname="postgres";
-        String password="naresh123";
+        String uname="username";
+        String password="password";
         //  creating connection
         Connection con=DriverManager.getConnection(url,uname,password);
         System.out.println("connection established");
